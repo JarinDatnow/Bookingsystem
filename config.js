f5fc3142eaff24543bf36caa0c17a836c6510379
JarinDatnow <129365960+JarinDatnow@users.noreply.github.com>
2026-08-19 11:46:54 +0200

    Update API_URL to the correct deployment URL

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
    Paste the Web app URL from your Apps Script deployment between the quotes.
    It looks like: https://script.google.com/macros/s/AKfy.../exec        */
 
-window.API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.API_URL = "https://script.google.com/macros/s/AKfycbzIQhJxoTzGT73UJhyV3JadXMnMdhF0YhqoXD0VGC8Yy0FwcknxGJDBbEwaynTwfTef/exec";
